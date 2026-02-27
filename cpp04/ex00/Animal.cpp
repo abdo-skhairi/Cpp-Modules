987a7eea8a4ab5fdbb55ea5a313f3d10828122aa
Skhairi Abderrahmane <sabderra@c1r4p9.1337.ma>
2026-02-27 12:17:34 +0000

    cpps

---
+++ cpp04/ex00/Animal.cpp
@@ -0,0 +1,34 @@
+#include "Animal.hpp"
+
+Animal::Animal()
+{
+    std::cout << "Animal " << type << " constructor called" << std::endl; 
+    type = "";
+}
+
+Animal::~Animal()
+{
+    std::cout << "Animal " << type << " destructor called" << std::endl;
+}
+
+
+Animal &Animal::operator=(const Animal &other)
+{
+    if(this != &other)
+    {
+        type = other.type;
+    }
+    std::cout << "Copy assignment operator called for " << std::endl;
+    return(*this);
+}
+
+Animal::Animal(const Animal &other)
+{
+    this->type = other.type;
+    std::cout << "Copy constructor called for " << type << std::endl;
+}
+
+void makeSound()
+{
+    std::cout << "Animal sound" << std::endl;
+}
